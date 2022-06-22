1909c30ff833489a8869416c51f1550acd1c0fee
Israr ulhaq <43034338+israr2018@users.noreply.github.com>
2022-06-22 11:48:44 +0500

    Update user.js
    swagger simple is added

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,53 @@ const express=require('express');
 const router=express.Router();
 // const cmsDto=require('../dto/mcc-dto');
 // const validateDto=require('../middleware/validate-dto')
+/**
+ * @swagger
+ *
+ * /feedback:
+ *   post:
+ *     tags:
+ *      -  Feedback
+ *     description: Share your feedback
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *      - name: body
+ *        in: body
+ *        schema:
+ *          type: object
+ *          required:
+ *          properties:
+ *              email:
+ *                  type: string
+ *              subject:
+ *                  type: string
+ *              body:
+ *                  type: string
+ *     responses:
+ *      200:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            status:
+ *              type: boolean
+ *              description: true
+ *            version:
+ *              type: string
+ *            message:
+ *              type: string
+ *              default: api message
+ *      '400':
+ *         description: Bad request.
+ *      '401':
+ *         description: Authorization information is missing or invalid.
+ *      '404':
+ *         description: A user with the specified token is not found.
+ *      '5XX':
+ *         description: Unexpected error.
+ */
 const UserController=require('../controllers/user-controller')
 router.post('/create',UserController.createUser);
 router.get('/get-by-id',UserController.getUserById);
